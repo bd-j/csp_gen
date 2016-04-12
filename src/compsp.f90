df913b1a0410dd04df67695a481d0fcd70ebee51
Benjamin Johnson <benjamin.duncan.johnson@gmail.com>
2016-04-12 09:54:16 -0400

    add compsp, move convert_params to csp_gen, add some text on the FSPS integrals

---
+++ src/compsp.f90
@@ -0,0 +1,127 @@
+SUBROUTINE COMPSP(write_compsp,nzin,outfile,&
+     mass_ssp,lbol_ssp,tspec_ssp,&
+     pset,ocompsp)
+
+
+  ! ------ Various checks and setup ------
+  
+  IF (check_sps_setup.EQ.0) THEN
+     WRITE(*,*) 'COMPSP ERROR: '//&
+          'SPS_SETUP must be run before calling COMPSP. '
+     STOP
+  ENDIF
+  !make sure various variables are set correctly
+  CALL COMPSP_WARNING(maxtime,pset,nzin,write_compsp)
+  !setup output files
+  if (pset%tage.gt.0) then
+     nage = 1
+  else
+     nage = ntfull
+  endif
+  IF (write_compsp.GT.0) &
+       CALL COMPSP_SETUP_OUTPUT(write_compsp,pset,outfile,1,nage)
+  
+  ! ------ Prepare SSPs ------
+  ! Dust attenuation + emission, nebular emission etc.
+  ! We should probably only do this for ages up to tage, if it is set.
+
+  spec_ssp = tspec_ssp
+  
+  ! Add nebular emission
+  if (add_neb_emission.EQ.1) then
+     if (nzin.GT.1) then
+        WRITE(*,*) 'COMPSP ERROR: cannot handle both nebular '//&
+             'emission and mult-metallicity SSPs in compsp'
+        STOP
+     endif
+     call add_nebular(pset,tspec_ssp(:,:,1),spec_ssp(:,:,1))
+  endif
+
+  ! Add dust emission
+  ! This is a wierd way to do this. Also not strictly correct, since the age
+  ! bin older than dust_tesc will include some contribution from young star
+  ! dust due to the interpolation, and changing dust_tesc by values smaller
+  ! than the ssp age grid will have no effect on the output.
+  ! The correct way is seprately calculate csp_spectra for the t<tesc and t >
+  ! tesc portions and feed to add_dust as intended.  However, this requires
+  ! significant extra logic (basically messing around with the integration
+  ! limits in sfh_weight) for probably little gain in accuracy.
+  if ((pset%dust1.gt.tiny_number).or.(pset%dust2.gt.tiny_number)) then
+     do i=1, ntfull
+        csp1 = 0.0
+        csp2 = 0.0
+        if (time_full(i).LT.pset%dust_tesc) then
+           csp1 = spec_ssp(:,i,1)
+        else
+           csp2 = spec_ssp(:,i,1)
+        endif
+        !add dust and combine young and old csp
+        call add_dust(pset,csp1,csp2,spec_dusty,mdust)
+        spec_ssp(:,i,1) = spec_dusty
+        mdust_ssp(i) = mdust
+     enddo
+  endif
+
+  ! --- Get CSP spectra -------
+  
+  ! loop over output ages
+  do i=1,ntfull
+     if (pset%tage.gt.0) then
+        ! A specific age was asked for, we will only compute one spectrum
+        age = pset%tage
+     else
+        ! Otherwise we will calculate composite spectra for each SSP age.
+        age = 10**(time_full(i)-9)
+     endif
+
+     ! -----
+     ! Get the spectrum for this age.  Note this is always normalized to one
+     ! solar mass formed, so we actually need to renormalize if computing all ages
+     call csp_gen(mass_ssp,lbol_ssp,spec_ssp,mdust_ssp,&
+          pset,age,&
+          mass_csp,lbol_csp,spec_csp,mdust_csp)     
+
+     ! -------
+     ! Now do a bunch of stuff with the spectrum
+     ! Smooth the spectrum
+     if (pset%sigma_smooth.GT.0.0) then
+        call smoothspec(spec_lambda,spec_csp,pset%sigma_smooth,&
+             pset%min_wave_smooth,pset%max_wave_smooth)
+     endif
+     ! Add IGM absorption
+     if (add_igm_absorption.EQ.1.AND.pset%zred.GT.tiny_number) then
+        spec_csp = igm_absorb(spec_lambda,spec_csp,pset%zred,&
+             pset%igm_factor)
+     endif
+     ! Compute spectral indices
+     if (write_compsp.EQ.4) then
+        call getindx(spec_lambda,spec_csp,indx)
+     else
+        indx=0.0
+     endif
+     ! Compute mags
+     if (redshift_colors.EQ.0) then
+        call getmags(pset%zred,spec_csp,mags,pset%mag_compute)
+     else
+        ! here we compute the redshift at the corresponding age
+        zred = min(max(linterp(cosmospl(:,2),cosmospl(:,1),&
+             10**time_full(i)/1E9),0.0),20.0)
+        call getmags(zred,spec_csp,mags,pset%mag_compute)
+     endif
+
+     ! ---------
+     ! Store the spectrum and write....
+     call save_compsp(write_compsp,ocompsp(i),log10(age)+9,&
+          mass_csp,lbol_csp,mags,spec_csp,mdust_csp,indx)
+
+     ! Terminate the loop if specific age was requested
+     if (pset%tage.gt.0) then
+        exit
+     endif
+     
+  enddo
+
+  if (write_compsp.EQ.1.OR.write_compsp.EQ.3) CLOSE(10)
+  if (write_compsp.EQ.2.OR.write_compsp.EQ.3) CLOSE(20)
+
+end subroutine compsp
--- src/csp_gen.f90
+++ src/csp_gen.f90
@@ -1,5 +1,4 @@
-subroutine csp_gen(write_compsp,nzin,outfile,mass_ssp,&
-     lbol_ssp,tspec_ssp,pset,ocompsp)
+subroutine csp_gen(mass_ssp,lbol_ssp,spec_ssp,pset,tage,csp_spec)
   !
   ! Return the spectrum of a composite stellar population
   !
@@ -9,62 +8,35 @@ subroutine csp_gen(write_compsp,nzin,outfile,mass_ssp,&
   ! sfh=4: delayed tau model
   ! sfh=5: custom SFH (see Simha et al. 2013)
 
-  use sps_vars
+  use sps_vars, only: ntfull
   implicit none
-
-  spec_ssp = tspec_ssp
   
-  ! ------ Prepare SSPs ------
-  ! Dust attenuation + emission, nebular emission etc.
-
-  ! Add nebular emission
-  IF (add_neb_emission.EQ.1) THEN
-     IF (nzin.GT.1) THEN
-        WRITE(*,*) 'COMPSP ERROR: cannot handle both nebular '//&
-             'emission and mult-metallicity SSPs in compsp'
-        STOP
-     ENDIF
-     CALL ADD_NEBULAR(pset,tspec_ssp(:,:,1),spec_ssp(:,:,1))
-  ENDIF
-
-  ! Add dust emission
-  ! This is a wierd way to do this
-  if ((pset%dust1.gt.tiny_number).or.(pset%dust2.gt.tiny_number)) then
-     do i=1, ntfull
-        csp1 = 0.0
-        csp2 = 0.0
-        IF (time_full(i).LT.pset%dust_tesc) THEN
-           csp1 = spec_ssp(:,i,1)
-        ELSE
-           csp2 = spec_ssp(:,i,1)
-        ENDIF
-        !add dust and combine young and old csp
-        CALL ADD_DUST(pset,csp1,csp2,spec_csp,mdust)
-        spec_ssp(:,i,1) = spec_csp
-     enddo
-  endif
+  ! Build a structure containing useful units, numbers, and switches for the
+  ! weight calculations
+  call convert_sfhparams(pset,tage,sfhpars)
      
   ! ----- Get SFH weights -----
-
-  ! Build a structure containing useful units and switches
-  call convert_sfhparams(pset, sfhpars)
   
   ! Tau and delayed-tau
   if ((pset%sfh.EQ.1).OR.(pset%sfh.EQ.4)) THEN
      sfhpars%type = pset%sfh
-     total_weights = sfh_weight(sfhpars, 1, ntfull)
+     ! only calculate SFH weights for SSPs up to tage (plus the next one)
+     imax = clip(locate(time_full, log10(sfhpars%tage)) + 1, 1, ntfull)
+     total_weights = sfh_weight(sfhpars, 1, imax)
      total_weights = total_weights / sum(total_weights)
   endif
 
   ! Simha
   if (pset%sfh.eq.5) then
+     ! only calculate SFH weights for SSPs up to tage (plus the next one)
+     imax = clip(locate(time_full, log10(sfhpars%tage)) + 1, 1, ntfull)
      ! delayed-tau model portion
      sfhpars%type = 4
-     w1 = sfh_weight(sfhpars, 1, ntfull)
+     w1 = sfh_weight(sfhpars, 1, imax)
      ! linear portion.  Need to set use_simha_limits flag to get corect limits
      sfhpars%type = 5
      sfhpars%use_simha_limits = 1
-     w2 = sfh_weight(sfhpars, 1, ntfull)
+     w2 = sfh_weight(sfhpars, 1, imax)
      sfhpars%use_simha_limits = 0
      ! normalize and sum.  need to be careful of divide by zero here, if all
      ! linear or all delay-tau
@@ -86,6 +58,7 @@ subroutine csp_gen(write_compsp,nzin,outfile,mass_ssp,&
 
   ! Tabular
   if (pset%sfh.EQ.2.OR.pset%sfh.EQ.3) then
+     total_weights = 0.
      call setup_tabular()
      ! linearly interpolate in the bins
      sfhpars%type = 5
@@ -94,8 +67,8 @@ subroutine csp_gen(write_compsp,nzin,outfile,mass_ssp,&
         ! mass formed in this bin assuming linear
         mass = (sfhtab(i,2) + sfhtab(i+1, 2)) / (2 * (sfhtab(i+1,1) - sfhtab(i, 1)))
         ! min and max ssps to consider
-        imin = locate(time_full, log10(sfhtab(i, 1))
-        imax = locate(time_full, log10(sfhtab(i+1, 1))
+        imin = clip(locate(time_full, log10(sfhtab(i, 1)) - 1, 1, ntfull)
+        imax = clip(locate(time_full, log10(sfhtab(i+1, 1)) + 1, 1, ntfull)
         ! set integration limits
         sfhpars%tq = sfhtab(i, 1)
         sfhpars%tage = sfhtab(i+1, 1)
@@ -104,19 +77,90 @@ subroutine csp_gen(write_compsp,nzin,outfile,mass_ssp,&
         w = sfh_weight(sfh_pars, imin, imax)
         total_weight = total_weight + w / sum(w) * mass
      enddo
-     
   endif
 
   ! Now weight each SSP by `total_weight` and sum
-
-  ! Add IGM absorption
+  csp_spec =
+  csp_mass =
+  csp_lbol = 
   
 end subroutine csp_gen
 
+subroutine convert_sfhparams(pset, tage, sfh)
+  ! Convert the pset values to yrs and pre-calculate some useful lookback
+  ! times, and store in a structure.  Note that this subroutine uses but does
+  ! not alter the pset values.
+  !
+  ! Inputs
+  ! ----------
+  !
+  ! pset:
+  !    The parameter set.
+  !
+  ! tage:
+  !    The age (in forward time, not lookback time) at which you are
+  !    calculating the composite spectrum, in Gyr.
+  !
+  ! Outputs
+  ! ----------
+  ! sfh:
+  !    An SFHPARAMS structure, containing SFH parameters in yrs, and some
+  !    useful lookback times.
+  !       - `tq` is the truncation time, in lookback time
+  !       - `t0` is the zero crossing time for a linear SFH, in lookback time.
+  !
+  use sps_vars, only: pset
+  implicit none
+  
+  type(PARAMS), intent(in) :: pset
+  real(SP), intent(in) :: tage
+  
+  type(SFHPARAMS), intent(inout) :: sfh
+
+  ! Convert units from Gyr to yr
+  sfh%tage = tage * 1e9
+  sfh%tau = pset%tau * 1e9
+  sfh%sf_trunc = pset%sf_trunc * 1e9
+  sfh%sf_slope = pset%sf_slope / 1e9
+
+  ! convert sf_trunc to to t_lookback
+  if ((sfh%sf_trunc.le.0).or.(sfh%sf_trunc.gt.sfh%tage)) then
+     sfh%tq = 0
+  else
+     sfh%tq = sfh%tage - sfh%sf_trunc
+  endif
+  ! get zero crossing time (in lookback time), avoiding divison by zero
+  if (abs(sfh%sf_slope).gt.tiny_number) then
+     sfh%t0 = sfh%tq - 1. / sfh%sf_slope
+  else
+     sfh%t0 = 0
+  endif
+  ! If the zero crossing time is outside the linear regime, set it to zero.
+  if ((sfh%t0.gt.sfh%tq).or.(sfh%t0.le.0)) then
+     sfh%t0 = 0
+  endif
+
+end subroutine convert_sfhparams
+
 
 function simha_norm(pset)
+  ! Get the relative normalization of the linear and delyed tau portions of the
+  ! simha SFH
+  !
+  
+  implicit none
+  type(PARAMS), intent(in) :: pset
+
+  real(SP) :: Tmax, Tz
+  real(SP) :: mass_tau, mass_linear
+  real(SP) :: m, sfr_q
+
+  real(SP) intent(out) :: simha_norm
+
+  m = pset%sf_slope
+  
   ! mass in the delayed tau portion
-  if ((pset%sf_trunc.le.0).or.(pset%sf_trunc.gt.tage)) then
+  if ((pset%sf_trunc.le.0).or.(pset%sf_trunc.gt.pset%tage)) then
      Tmax = pset%tage
   else
      Tmax = pset%sf_trunc
@@ -128,14 +172,17 @@ function simha_norm(pset)
   
   ! Mass in the linear portion.
   ! This is integral of (1 - m * (T - Tmax)) from Tmax to Tzero
-  Tz = Tmax + 1/np.float64(sf_slope) ! need to deal with divide by zero
-  if ((Tz.lt.Tmax).or.(Tz.gt.pset%tage).or.(pset%sf_slope.eq.0)) then
+  if (m.gt.0) then
+     Tz = Tmax + 1.0/m
+  else
      Tz = pset%tage
   endif
-  m = pset%sf_slope
+  if ((Tz.lt.Tmax).or.(Tz.gt.pset%tage)) then
+     Tz = pset%tage
+  endif
   mass_linear = (Tz - Tmax) - m/2.*(Tz**2 + Tmax**2) + m*Tz*Tmax
   
   ! now compute normalization relative to 1 Msun in the delayed-tau portion
-  simha_norm = mass_linear * sfr_q / mass_tau
+  simha_norm = sfr_q * (mass_linear / mass_tau)
   
 end function simha_norm
